96b24dec906031f13f6c6a94968319c9d2913467
Raffaele Marino <44016352+RaffaeleMarino@users.noreply.github.com>
2024-09-17 15:57:54 +0200

    Add files via upload

---
+++ 3-SAT-N=10/anal_for_maxsat.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=np.loadtxt("output_maxsatK=3.txt")
+x=data[:,0]
+y=data[:,1]
+z=data[:,2]
+
+mean=np.mean(y/z)
+std=np.std(y/z)/np.sqrt(z.shape[0])
+print(mean, std)
